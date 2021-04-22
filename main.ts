54e3a6f28dd2f65ad2aba82955d2c723aefb0fcd
STEM-Power <81220298+STEM-Power@users.noreply.github.com>
2021-04-22 21:51:35 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -16,13 +16,12 @@ namespace UART {
     //% blockId="RXdata" block="UART RX Data"
     //% weight=90 blockGap=8
     export function RXdata(): string {
-        let result: boolean = false
+
         let response: string = ""
         serial.onDataReceived(serial.delimiters(Delimiters.NewLine), function () {
             response += serial.readString()
-            if (response.length > 200) {
-                response = response.substr(response.length - 200)
-            }
+
+            
         })
         serial.onDataReceived(serial.delimiters(Delimiters.NewLine), () => { })
         return response
